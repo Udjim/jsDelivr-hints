bc6f5d37de2a4506197dff204758797bb1e5ccbd
Evgenii Egorov <evgenii.egorov@outlook.com>
2023-07-29 20:50:27 +0300

    Initial commit

---
+++ base/load-new-script.js
@@ -0,0 +1,4 @@
+script = document.createElement('script');
+script.type = 'text/javascript';
+script.src = 'https://cdn.jsdelivr.net/gh/Udjim/telegram-data-fetcher/test.js';
+document.getElementsByTagName('head')[0].appendChild(script);
