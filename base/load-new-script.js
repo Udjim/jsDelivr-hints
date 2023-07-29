da6cc5da47e10e6a05164c741428f40307232f09
Evgenii Egorov <evgenii.egorov@outlook.com>
2023-07-29 21:14:34 +0300

    add logic

--- base/load-new-script.js
+++ base/load-new-script.js
@@ -1,4 +1,14 @@
-script = document.createElement('script');
-script.type = 'text/javascript';
-script.src = 'https://cdn.jsdelivr.net/gh/Udjim/telegram-data-fetcher/test.js';
-document.getElementsByTagName('head')[0].appendChild(script);
+//replace script.src
+//replace .p2
+(function(){if (typeof window.p1 === 'undefined') {
+    script = document.createElement('script');
+    script.type = 'text/javascript';
+    script.src = 'https://cdn.jsdelivr.net/gh/Udjim/jsdelivr-hints/base/source-js-template.js';
+    document.getElementsByTagName('head')[0].appendChild(script);
+    console.log("Script has been loaded");
+  }
+  else {
+    console.log("Using saved script", window.p1);
+    window.p1();
+}
+})();
---
+++ base/source-js-template.js
@@ -0,0 +1,11 @@
+//replace .p2
+//replace function BODY in  window.p1 = function () { ... replace this ... }
+//remove console.log(window.p1);
+(function(){
+    console.log(window.p1);
+    window.p1 = function () {
+  console.log("Yeap, worked!");
+      };
+
+    window.p1();
+})();
