f1c29f5fcca1eae3816e8d56625804bf7baa0870
Evgenii Egorov <evgenii.egorov@outlook.com>
2023-08-02 18:31:32 +0300

    add bhe

--- base/load-new-script.js
+++ base/load-new-script.js
@@ -1,14 +1,28 @@
 //replace script.src
 //replace .p2
 (function(){if (typeof window.p1 === 'undefined') {
-    script = document.createElement('script');
-    script.type = 'text/javascript';
-    script.src = 'https://cdn.jsdelivr.net/gh/Udjim/jsdelivr-hints/base/source-js-template.js';
-    document.getElementsByTagName('head')[0].appendChild(script);
+    s = document.createElement('script');
+    s.crossOrigin = "anonymous";
+    s.type = 'text/javascript';
+    s.src = 'https://cdn.jsdelivr.net/gh/Udjim/jsdelivr-hints/base/source-js-template.js';
+    document.getElementsByTagName('head')[0].appendChild(s);
     console.log("Script has been loaded");
   }
   else {
-    console.log("Using saved script", window.p1);
-    window.p1();
+    console.log("Using saved script");
 }
+    window.p1();
+
+})();
+
+//replace script.src
+//replace .p2
+(function(){if (typeof window.bhe === 'undefined') {
+    s = document.createElement('script');
+    s.crossOrigin = "anonymous";
+    s.type = 'text/javascript';
+    s.src = 'https://cdn.jsdelivr.net/gh/Udjim/ti/parsers/bhe.js';
+    document.getElementsByTagName('head')[0].appendChild(s);
+  }
+    window.bhe();
 })();
--- base/source-js-template.js
+++ base/source-js-template.js
@@ -1,11 +1,12 @@
 //replace .p2
 //replace function BODY in  window.p1 = function () { ... replace this ... }
 //remove console.log(window.p1);
-(function(){
-    console.log(window.p1);
-    window.p1 = function () {
-  console.log("Yeap, worked!");
-      };
+(function () {
+  console.log(window.p1);
+  window.p1 = function () {
+    console.log("Yeap, worked!");
+  };
 
-    window.p1();
+  window.p1();
 })();
+
