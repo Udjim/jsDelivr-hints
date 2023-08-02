45e9ec29b46ebd6ff12ce8741cd604a6efaf328e
Evgenii Egorov <evgenii.egorov@outlook.com>
2023-08-02 18:38:51 +0300

    fix error

--- base/load-new-script.js
+++ base/load-new-script.js
@@ -1,28 +1,26 @@
-//replace script.src
-//replace .p2
-(function(){if (typeof window.p1 === 'undefined') {
-    s = document.createElement('script');
-    s.crossOrigin = "anonymous";
-    s.type = 'text/javascript';
-    s.src = 'https://cdn.jsdelivr.net/gh/Udjim/jsdelivr-hints/base/source-js-template.js';
-    document.getElementsByTagName('head')[0].appendChild(s);
-    console.log("Script has been loaded");
-  }
-  else {
-    console.log("Using saved script");
-}
-    window.p1();
-
-})();
 
 //replace script.src
-//replace .p2
-(function(){if (typeof window.bhe === 'undefined') {
-    s = document.createElement('script');
-    s.crossOrigin = "anonymous";
-    s.type = 'text/javascript';
-    s.src = 'https://cdn.jsdelivr.net/gh/Udjim/ti/parsers/bhe.js';
-    document.getElementsByTagName('head')[0].appendChild(s);
-  }
-    window.bhe();
-})();
+//replace .bhe
+(function() {
+    if (typeof window.bhe === 'undefined') {
+      var s = document.createElement('script');
+      s.crossOrigin = "anonymous";
+      s.type = 'text/javascript';
+      s.src = 'https://cdn.jsdelivr.net/gh/Udjim/ti/parsers/bhe.js';
+  
+      // Define a function to be called when the script is loaded
+      s.onload = function() {
+        window.bhe(); // Call the function after the script is loaded
+      };
+  
+      document.getElementsByTagName('head')[0].appendChild(s);
+    } else {
+      window.bhe(); // Call the function directly if it's already defined
+    }
+  })();
+
+
+
+//https://jscompress.com
+//https://chat.openai.com/c/22e94ff8-11db-4aeb-9a5f-8e0b68484b97
+//minify compress and create from this js a js that can be added as a bookmark, it must start with 'javacsript:'
