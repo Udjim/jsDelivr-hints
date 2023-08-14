e46461393c1907a4f0a668bacc21238f7c699d7f
Evgenii Egorov <evgenii.egorov@outlook.com>
2023-08-14 23:48:44 +0300

    add comment

--- base/load-new-script.js
+++ base/load-new-script.js
@@ -16,4 +16,5 @@
 
 //https://jscompress.com
 //https://chat.openai.com/c/22e94ff8-11db-4aeb-9a5f-8e0b68484b97
+//chatGPT promt:
 //minify compress and create from this js a js that can be added as a bookmark, it must start with 'javacsript:'
